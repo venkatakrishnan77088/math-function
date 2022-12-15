4aca7bf4232ff6f063af5fa48d9932a32ecb21d5
venkatakrishnan77088 <vkproplayer1234@gmail.com>
2022-12-15 19:46:42 +0530

    math-function

---
+++ Math functioon/js/js.js
@@ -0,0 +1,26 @@
+//----JS math----
+//x = Math.sqrt(676);
+//x = Math.cbrt(125); 
+//x = Math.abs(-90.09);
+//x = Math.random();
+//x = Math.floor(Math.random()*10);
+//x = Math.random()*10;
+//x = Math.pow(2,4); //(m,n)
+//x = Math.floor(-5.9); //returns the lowest integer
+//x = Math.ceil(-9.4); //returns the highest integer
+//x = Math.round(8.5); 
+//x = Math.acos(1);
+//x = Math.acos(0);
+//x = Math.acos(-1);
+//x = Math.trunc(89.7899);
+//x = Math.sign(-0); //positive 1, negative -1, zero 0
+//x = Math.PI;
+//x = Math.max(76,78,98);
+//x = Math.min(76,78);
+//x = Math.log(7);
+//x = Math.fround(7.3);
+//console.log(x)
+
+
+
+
